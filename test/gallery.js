2a3f691d99d69d1c9f627f1884f10c47d9f404fb
Mike_Device <mike.device.094@gmail.com>
2017-02-01 22:43:41 +0300

    add src, tests and linter

---
+++ test/gallery.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var Gallery = require('../src/gallery'),
+	expect = require('expect.js');
+
+describe('Gallery test', function() {
+	it('example', function() {
+		expect(Gallery).to.be.ok();
+	});
+});
