81d1c4919681e8a719e5df2f7b6324f9d2f02d3b
Mike_Device <mike.device.094@gmail.com>
2017-02-01 23:11:05 +0300

    add empty methods

--- src/gallery.js
+++ src/gallery.js
@@ -1,5 +1,24 @@
 'use strict';
 
-var Gallery = function() {};
+var Gallery = function() {
+	this.images = [];
+	this.currentImageIndex = 0;
+};
+
+Gallery.prototype.setCurrentImage = function(index) {
+	var total = this.images.length;
+
+	this.currentImageIndex = index;
+};
+
+Gallery.prototype.prev = function() {};
+
+Gallery.prototype.next = function() {};
+
+Gallery.prototype.add = function() {};
+
+Gallery.prototype.remove = function() {};
+
+Gallery.prototype.removeAll = function() {};
 
 module.exports = Gallery;
